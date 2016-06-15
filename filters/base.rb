010b7fb7d7fb90c81d1b125ce6c300ed77f7f6cb
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2016-06-15 15:16:10 +1000

    Replace all {{ base }}

--- filters/base.rb
+++ filters/base.rb
@@ -5,7 +5,7 @@ module Jekyll
   module BaseFilter
     def relativity(input)
       # It can either replace the {{ base }} variable
-      if input.start_with?('{{ base }}/')
+      if input.include? "{{ base }}"
         url = @context.registers[:page]['url'].chomp('/').reverse.chomp('/').reverse.split('/')
 
         if url.length > 0 and File.extname(url[url.length-1]) != ""
